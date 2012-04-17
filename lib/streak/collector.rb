882cb21c7ee9a2672554e059a14049855eaf8ac4
David Czarnecki <czarneckid@acm.org>
2012-04-16 22:04:38 -0400

    Updates
    * Allow for aggregate method to take an arbitrary positive or negative count
    * Added statistics method to be able to retrieve all or some combination of statistics

--- lib/streak/collector.rb
+++ lib/streak/collector.rb
@@ -11,10 +11,10 @@ module Streak
         previous_streak = previous_data[1].to_i
 
         Streak.redis.multi do |transaction|
-          transaction.set("#{Streak::namespace}::#{Streak::wins_streak_key}::#{id}", [previous_wins + 1, previous_streak].max)
-          transaction.incrby("#{Streak::namespace}::#{Streak::wins_key}::#{id}", 1)
+          transaction.set("#{Streak::namespace}::#{Streak::wins_streak_key}::#{id}", [previous_wins + count, previous_streak].max)
+          transaction.incrby("#{Streak::namespace}::#{Streak::wins_key}::#{id}", count.abs)
           transaction.set("#{Streak::namespace}::#{Streak::losses_key}::#{id}", 0)
-          transaction.incrby("#{Streak::namespace}::#{Streak::total_key}::#{id}", 1)
+          transaction.incrby("#{Streak::namespace}::#{Streak::total_key}::#{id}", count.abs)
         end
       else
         previous_data = Streak.redis.multi do |transaction|
@@ -26,12 +26,20 @@ module Streak
         previous_streak = previous_data[1].to_i
 
         Streak.redis.multi do |transaction|
-          transaction.set("#{Streak::namespace}::#{Streak::losses_streak_key}::#{id}", [previous_losses + 1, previous_streak].max)
-          transaction.incrby("#{Streak::namespace}::#{Streak::losses_key}::#{id}", 1)
+          transaction.set("#{Streak::namespace}::#{Streak::losses_streak_key}::#{id}", [previous_losses + (count.abs), previous_streak].max)
+          transaction.incrby("#{Streak::namespace}::#{Streak::losses_key}::#{id}", count.abs)
           transaction.set("#{Streak::namespace}::#{Streak::wins_key}::#{id}", 0)
-          transaction.incrby("#{Streak::namespace}::#{Streak::total_key}::#{id}", 1)
+          transaction.incrby("#{Streak::namespace}::#{Streak::total_key}::#{id}", count.abs)
         end
       end
+    end
+
+    def statistics(id, keys = [Streak.wins_key, Streak.wins_streak_key, Streak.losses_key, Streak.losses_streak_key, Streak.total_key])
+      Streak.redis.multi do |transaction|
+        keys.each do |key|
+          transaction.get("#{Streak::namespace}::#{key}::#{id}")
+        end
+      end.map(&:to_i)
     end
   end
 end
--- spec/streak/collector_spec.rb
+++ spec/streak/collector_spec.rb
@@ -23,14 +23,32 @@ describe Streak::Collector do
       streak_value_for(Streak.losses_key, 'david').should == 1
       streak_value_for(Streak.losses_streak_key, 'david').should == 1
       streak_value_for(Streak.total_key, 'david').should == 3
-      Streak.aggregate('david', 1)
-      Streak.aggregate('david', 1)
-      Streak.aggregate('david', 1)
+      Streak.aggregate('david', 3)
       streak_value_for(Streak.wins_key, 'david').should == 3
       streak_value_for(Streak.wins_streak_key, 'david').should == 3
       streak_value_for(Streak.losses_key, 'david').should == 0
       streak_value_for(Streak.losses_streak_key, 'david').should == 1
       streak_value_for(Streak.total_key, 'david').should == 6
+    end    
+  end
+
+  describe '#statistics' do
+    it 'should return the default list of statistics if no keys list is provided' do
+      Streak.aggregate('david', 3)
+      Streak.aggregate('david', -2)
+      Streak.aggregate('david', 5)
+      Streak.aggregate('david', -1)
+
+      Streak.statistics('david').should == [0, 5, 1, 2, 11]
+    end
+
+    it 'should return the specified list of statistics if a keys list is provided' do
+      Streak.aggregate('david', 3)
+      Streak.aggregate('david', -2)
+      Streak.aggregate('david', 5)
+      Streak.aggregate('david', -1)
+
+      Streak.statistics('david', [Streak.wins_streak_key, Streak.losses_streak_key]).should == [5, 2]
     end
   end
 end
