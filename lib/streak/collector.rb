8f8c44de3bbaff88cbb93158e7cd703d5932e96a
David Czarnecki <czarneckid@acm.org>
2012-04-17 20:23:38 -0400

    Updating documentation

--- lib/streak/collector.rb
+++ lib/streak/collector.rb
@@ -1,5 +1,16 @@
 module Streak
   module Collector
+    # Aggregate streaks for a given +id+. If +count+ is greater than 0, it will increment +Streak.positive_key+ and 
+    # +Streak.positive_total_key+ by the absolute value of count. It will zero out +Streak.negative_key+. Finally, it 
+    # will add the absolute value of count to +Streak.total_key+. If the current positive streak is greater than 
+    # the value of +Streak.positive_streak_key+, its value will be replaced. 
+    # If +count+ is less than than 0, it will increment +Streak.negative_key+ and 
+    # +Streak.negative_total_key+ by the absolute value of count. It will zero out +Streak.positive_key+. Finally, it 
+    # will add the absolute value of count to +Streak.total_key+. If the current negative streak is greater than 
+    # the value of +Streak.negative_streak_key+, its value will be replaced.
+    #
+    # @param id [String] ID of the item being monitored for a streak.
+    # @param count [Integer] Streak count, which can be positive or negative.
     def aggregate(id, count)
       if count >= 0
         previous_data = Streak.redis.multi do |transaction|
@@ -27,7 +38,7 @@ module Streak
         previous_streak = previous_data[1].to_i
 
         Streak.redis.multi do |transaction|
-          transaction.set("#{Streak::namespace}::#{Streak::negative_streak_key}::#{id}", [previous_losses + (count.abs), previous_streak].max)
+          transaction.set("#{Streak::namespace}::#{Streak::negative_streak_key}::#{id}", [previous_losses + count.abs, previous_streak].max)
           transaction.incrby("#{Streak::namespace}::#{Streak::negative_key}::#{id}", count.abs)
           transaction.incrby("#{Streak::namespace}::#{Streak::negative_total_key}::#{id}", count.abs)
           transaction.set("#{Streak::namespace}::#{Streak::positive_key}::#{id}", 0)
@@ -36,6 +47,17 @@ module Streak
       end
     end
 
+    # Retrieve all (or some) of the streak statistics collected. By default, without a second parameter, this 
+    # method will return a +Hash+ of: +Streak.positive_key+, +Streak.positive_total_key+, +Streak.positive_streak_key+, 
+    # +Streak.negative_key+, +Streak.negative_total_key+, +Streak.negative_streak_key+, and +Streak.total_key+ with
+    # their corresponding values. If you want a subset of that list, pass in an array with the keys you want
+    # returned.
+    # 
+    # @param id [String] ID.
+    # @param keys [Array, optional]. Optional list of streak statistic keys to be retrieved.
+    #
+    # @return +Hash+ of streak statistics and their corresponding values for a given +id+.
+    #
     def statistics(id, keys = [Streak.positive_key, Streak.positive_total_key, Streak.positive_streak_key, Streak.negative_key, Streak.negative_total_key, Streak.negative_streak_key, Streak.total_key])
       values = Streak.redis.multi do |transaction|
         keys.each do |key|
@@ -46,6 +68,9 @@ module Streak
       Hash[keys.map(&:to_sym).zip(values)]
     end
 
+    # Reset all the statistics for a given +id+ to 0.
+    #
+    # @param id [String] ID.
     def reset_statistics(id)
       Streak.redis.multi do |transaction|
         [Streak.positive_key, Streak.positive_total_key, Streak.positive_streak_key, Streak.negative_key, Streak.negative_total_key, Streak.negative_streak_key, Streak.total_key].each do |key|
